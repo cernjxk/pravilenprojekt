4904d6deba2b7e94ad6b3ceeddb7f5724813fe67
nikstrmcnik <nik.strmcnik@student.um.si>
2026-01-05 14:21:46 +0100

    Add Identity (Register/Login) + LoginPartial in layout

--- PravilenProjekt/Program.cs
+++ PravilenProjekt/Program.cs
@@ -1,18 +1,37 @@
 using PravilenProjekt.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PravilenProjekt.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Connection string 
+var connectionString =
+    builder.Configuration.GetConnectionString("ApplicationDbContextConnection")
+    ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+// DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+// Identity (brez obvezne potrditve e-maila)
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<GameService>();
-var app = builder.Build();
 
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -21,6 +40,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// NUJNO za Identity
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
